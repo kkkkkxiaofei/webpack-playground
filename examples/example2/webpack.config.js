6fe734ba7905209f465c9e800d20e1c375d64282
kkkkkxiaofei <xifzhang@thoughtworks.com>
2020-06-21 11:53:26 +0800

    setup react compile context

---
+++ examples/example2/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  
+  mode: 'development'
+};
